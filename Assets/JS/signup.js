eb91a8a0dc92223283d911bb614eedac9a2c71c6
A.Yassine <ayassine.auce@gmail.com>
2023-08-23 20:11:08 +0300

    as is

--- Assets/JS/signup.js
+++ Assets/JS/signup.js
@@ -1,4 +1,5 @@
 let LoginbtnEl = document.getElementById('Loginbtn')
+let RegisterUserEl = document.getElementById('RegisterUser')
 let RegisterPassEl = document.getElementById('RegisterPass')
 let RegisterVerifyPassEl = document.getElementById('RegisterVerifyPass')
 let RegiterSubmitbtnEl = document.getElementById('RegiterSubmitbtn')
@@ -8,8 +9,9 @@ let UserPassword
 RegiterSubmitbtnEl.addEventListener('mouseover', function(){
     if(RegisterPassEl.value == RegisterVerifyPassEl.value && RegisterPassEl.value !=="") {
         // alert('correct')
-        UserName == 
+        UserName == RegisterUserEl.value
         UserPassword == RegisterPassEl.value
+        alert(UserName, UserPassword)
     }
 })
 document.title = "Sign Up"
