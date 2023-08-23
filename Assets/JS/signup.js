187c042fdc77663f1a3b1870cda975868a58297e
A.Yassine <ayassine.auce@gmail.com>
2023-08-23 20:01:45 +0300

    make script

--- Assets/JS/signup.js
+++ Assets/JS/signup.js
@@ -1,4 +1,14 @@
 let LoginbtnEl = document.getElementById('Loginbtn')
+let RegisterPassEl = document.getElementById('RegisterPass')
+let RegisterVerifyPassEl = document.getElementById('RegisterVerifyPass')
+let RegiterSubmitbtnEl = document.getElementById('RegiterSubmitbtn')
+let RegiterLoginbtnEl = document.getElementById('RegiterLoginbtn')
+
+RegiterSubmitbtnEl.addEventListener('hover', function(){
+    if(RegisterPassEl.value == RegisterVerifyPassEl.value){
+        alert('correct')
+    }
+})
 document.title = "Sign Up"
 LoginbtnEl.addEventListener('click', function(){
     window.location.replace("index.html")
